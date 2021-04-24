e07600448aa45f3eed4b1892212721863f586415
Chutchapon <chutchapon.soyklum@hotmail.com>
2021-04-24 10:15:21 +0700

    edit worng ;

--- class_24042021/LoopDemo.java
+++ class_24042021/LoopDemo.java
@@ -5,13 +5,14 @@ public class LoopDemo {
         int i = 1;
         /* int a = 5, i = 1; start */
         /* do { */
-            /* while (a < 3) { */
-            /* while (a < 10) { */
-            /* System.out.println("Line " + i++ + " : " + a);
-            a = a + 0.5; */
-            /* a = a + 1; step */
+        /* while (a < 3) { */
+        /* while (a < 10) { */
+        /*
+         * System.out.println("Line " + i++ + " : " + a); a = a + 0.5;
+         */
+        /* a = a + 1; step */
         /* } while (a < 2); stop */
-        for (int x =1 ; x<5 ; x++);
-            System.out.println("x = " + x);
+        for (int x = 1; x < 5; x++)
+        System.out.println("x = " + x);
     }
 }
