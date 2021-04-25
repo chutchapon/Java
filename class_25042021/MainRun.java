dfe74ba275caf428d0b5aef00f9f786bd0efa567
Chutchapon <chutchapon.soyklum@hotmail.com>
2021-04-25 10:13:37 +0700

    Modified MainRun.java

--- class_25042021/MainRun.java
+++ class_25042021/MainRun.java
@@ -10,6 +10,7 @@ public class MainRun {
         obj1.lineA();
         obj1.x = 10; /* obj1 Attribute x */
         obj1.y = 20; /* obj1 Attribute y */
+        obj1.z = 0;
         System.out.println("x+y = " + (obj1.x + obj1.y));
         /* can create and build */
         SuperClass obj2 = new SuperClass();
@@ -17,11 +18,20 @@ public class MainRun {
         obj2.x = 100;
         obj2.y = 200;
         obj2.z = obj2.x + obj2.y;
+        /* report */
         System.out.println("obj2.x+obj2.y = " + obj2.z);
         System.out.println("obj1.x = " + obj1.x + " " + "obj2.x = " + obj2.x);
         System.out.println("obj2.z = " + obj2.z);
         System.out.println(obj1);
         System.out.println(obj1.add());
         obj1.plus(5);
+        /* test input and cal */
+        // Scanner value = new Scanner(System.in);
+        // System.out.print("Enter Number for x : ");
+        // obj1.x = value.nextInt();
+        // System.out.print("Enter Number for y : ");
+        // obj1.y = value.nextInt();
+        // obj1.lineA();
+        // System.out.println(obj1.add());
     }
 }
