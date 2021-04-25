04c8c24f518af2b76e230414768354f9069fc1f4
Chutchapon <chutchapon.soyklum@hotmail.com>
2021-04-25 10:15:34 +0700

    Modified SuperClass.java & SuperClass.class

--- class_25042021/SuperClass.java
+++ class_25042021/SuperClass.java
@@ -1,10 +1,33 @@
-public class SuperClass{
+public class SuperClass {
     /* Attribute */
-    int x,y,z;
+    int x, y, z;
+
+    /* Constructor Method */
+    SuperClass() {
+        this.x = -1;
+        this.y = -2;
+        this.z = -3;
+    }
+
+    /* toString */
+    public String toString() {
+        return "this is SuperClass";
+    }
 
     /* Method */
-    void lineA(){
+    void lineA() {
         /* Method Body */
         System.out.println("----------------------");
+    }
+
+    int add() {
+        return this.x + this.y;
+    }
+
+    void plus(int x) {
+        System.out.println("input x = " + x);
+        /* form x recent in method */
+        System.out.println("input this.x = " + this.x);
+        /* Attribute in class */
     }
 }
