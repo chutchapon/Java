c973bac34ff9ae0188c5ddbd27d725286cf05db4
Chutchapon <chutchapon.soyklum@hotmail.com>
2021-04-25 09:23:03 +0700

    Modified MainRun.java

--- class_25042021/MainRun.java
+++ class_25042021/MainRun.java
@@ -2,7 +2,14 @@ public class MainRun {
 
     public static void main(String[] args) {
         SuperClass obj1;
+        /* create SuperClass */
         obj1 = new SuperClass();
+        /* create Object Superclass */
         obj1.lineA();
+        obj1.x = 10; /* obj1 Attribute x */
+        obj1.y = 20; /* obj1 Attribute y */
+        System.out.println("x+y = " + (obj1.x + obj1.y));
+        /* can create and build */
+        SuperClass obj2 = new SuperClass();
     }
 }
