4d8230e46f7aa7289fcb73ecf8d9c2e3c80e77ca
Chutchapon <chutchapon.soyklum@hotmail.com>
2021-05-01 10:13:26 +0700

    add Authentication file

---
+++ class_01052021/Authentication.java
@@ -0,0 +1,14 @@
+abstract public class Authentication {
+    private String userName;
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    abstract boolean login(String userName);
+    // abstract void login();
+}
